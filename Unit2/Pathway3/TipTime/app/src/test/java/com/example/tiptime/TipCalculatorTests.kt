000d59996cb4aee7050074ff014dc9110b0f8da4
김태훈 <50002966+thoon-kim@users.noreply.github.com>
2022-11-13 23:00:10 +0900

    Unit2_Pathway3_TipTime 자동 테스트 작성

---
+++ Unit2/Pathway3/TipTime/app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,16 @@
+package com.example.tiptime
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculate_20_percent_tip_no_roundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = "₩2"
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
